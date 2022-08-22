980d4642d6eba70b37fb78d3ddde58151345cabb
pradeepvd <93397045+pradeepvd@users.noreply.github.com>
2022-08-22 13:20:57 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int main()
+{
+int num,r,sum=0,temp;
+printf("Enter the number");
+scanf("%d",&num);
+temp =num;
+while(num > 0)
+{
+    r=num%10;
+    sum =sum*10+r;
+
+    num= num/10;
+}
+printf("the sum is= %d\n",sum);
+if(sum == temp)
+    printf("it is palindrome");
+else
+    printf("not a palindrome");
+return 0;
+}
+*/
